696f6bbe287548aa88e3f81ecf968d0fc72fffb4
psyphh <psyphh@gmail.com>
2021-06-23 14:44:22 +0800

    prepare 0.1.1

--- xifa/__init__.py
+++ xifa/__init__.py
@@ -3,4 +3,7 @@ from .base import Ordinal
 from .gpcm import GPCM
 from .grm import GRM
 
-__all__ = ["Base", "Ordinal", "GRM", "GPCM"]
+__all__ = ["Base",
+           "Ordinal",
+           "GRM",
+           "GPCM"]
